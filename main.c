bc70bf7af96b6b53ce5c3de7bb04ac3a5ba25ed9
Andrew Koenig <andrewkoenig04@gmail.com>
2025-10-14 10:47:57 -0300

    criando função seletor (organização)

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-int main(){
-  int a = 0;
+int seletor(int a);
 
+int main(){
+  seletor(0);
+}
+
+int seletor(int a){
   printf("0 - Sair\n");
   printf("1 - Criptografar\n");
   printf("2 - Descriptografar\n");
-    printf("Escolha uma opção: ");
+  printf("Escolha uma opção: ");
   scanf("%d", &a);
 
   switch (a){
@@ -14,6 +18,32 @@ int main(){
     printf("Saindo...\n");
     break;
   case 1:
+    printf("CRIPTOGRAFIA:\n");
+    printf("0 - Sair\n");
+    printf("1 - Cifra de Cesar\n");
+    printf("2 - Rafael\n");
+    printf("3 - Andrew\n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &a);
+    switch (a){
+      case 0:
+        printf("Saindo...\n");
+        break;
+      case 1:
+        printf("Cifra de Cesar\n");
+        break;
+    case 2:
+        printf("Rafael\n");
+        break;
+    case 3:
+      printf("Andrew\n");
+      break;
+    default:
+      printf("Opção inválida\n");
+      break;
+    }
+  case 2:
+    printf("DESCRIPTOGRAFIA:\n");
     printf("0 - Sair\n");
     printf("1 - Cifra de Cesar\n");
     printf("2 - Rafael\n");
